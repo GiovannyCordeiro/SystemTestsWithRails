14b47ea1da11b5f534c9665d25d2c5b4b1f59a99
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-04-28 22:56:00 -0300

    feat: adding interaction

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 
   get 'landing_page', to: 'landing_page#index', as: :landing_page
 end
--- spec/system/landing_page_spec.rb
+++ spec/system/landing_page_spec.rb
@@ -6,5 +6,14 @@ RSpec.describe "LandingPageSystem", type: :feature do
       visit '/landing_page'
       expect(page).to have_content('Landing Page')
     end
+
+    it "allows to submit" do
+      visit '/landing_page'
+      fill_in 'name', with: 'giovanny cordeiro'
+      fill_in 'number', with: 50
+      click_button 'Submit'
+
+      expect(current_path).to eq(root_path)
+    end
   end
 end
