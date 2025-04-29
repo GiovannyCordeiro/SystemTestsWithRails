828e513933992b8bc21b87b4b132d6aaa96f788d
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-04-28 22:52:55 -0300

    feat: adding new inputs

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  // connect() {
+  //   this.element.textContent = "Hello World!"
+  // }
+
+  static targets = ["name", "number"]
+
+  greet(event){
+    if(parseInt(this.numberTarget.value) > 100){
+      event.preventDefault();
+      return;
+    }
+    window.location.replace("/");
   }
 }
